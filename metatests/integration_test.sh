2b5bc88bc0ee13d7d58ca47990f88b8d60781c0d
Jared Claypoole <jared.claypoole@gmail.com>
2019-09-10 18:50:07 -0400

    Improve integration test documentation
    * Also mention unit tests in metatests/readme.md

--- metatests/integration_test.sh
+++ metatests/integration_test.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
+
+## Script to run integration tests (see metatests/readme.md)
+##	* Intended to be run in a docker container simulating
+##		Gradescope's autograder, as defined in metatests/Dockerfile
+
 cd /autograder
 
-mkdir results 
+# Copy the sample submission into the correct directory
 cp -r source/sample_submission/ submission/ 
+
+# Create the directory where results.json will live
+mkdir results 
+
+# Run the (simulated) autograder
 ./run_autograder 
+
+# Print the autograder results for easy inspection
 cat results/results.json
 
+# Compare the results against the expected results
+#	* Will return nonzero if the results differ
 python3 source/metatests/compare_autograder_results.py \
 	results/results.json \
 	submission/results_expected.json
--- metatests/readme.md
+++ metatests/readme.md
@@ -1,9 +1,23 @@
-## Dir `metatests`
+# Dir `metatests`
 
 Directory to hold tests of the autograder (and utilities) itself.
 
 Not to be confused with the `tests` directory, 
 which contains unit tests for students' submissions;
 i.e., the actually autograding.
+
+## Types of (meta)tests
+
+### Unit (meta)tests
+
+These test the code itself and run directly in this repository
+
+### Integration tests
+
+* These ensure the autograder is behaving as expected
+* These run inside a Docker container which mimics the one used by Gradescope
+* A python script, `metatests/compare_test_results.py`, is used to compare
+	simulated autograder results against the expected results,
+	by examining the results.json output
 
 
